440f668c5ec6b32e5974c9fefe6ec28b7f2e5d4a
Buitron <ivanandresbuve@unicauca.edu.co>
2020-10-11 09:17:35 -0500

    primera consulta funcionando

--- src/main/java/control/Concesionario.java
+++ src/main/java/control/Concesionario.java
@@ -21,7 +21,7 @@ public class Concesionario {
             MongoClientURI uri = new MongoClientURI("mongodb://userLab7:passworduserLab7@93.188.167.110:27017/?authSource=lab7");
             Cliente = new MongoClient(uri);
             MongoDatabase db;
-            db = Cliente.getDatabase("Lab7");
+            db = Cliente.getDatabase("lab7");
             MongoCollection<org.bson.Document> Vehiculo = db.getCollection("Carros");
             return Vehiculo.find().first().toJson();
           
